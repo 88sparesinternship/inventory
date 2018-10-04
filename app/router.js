f99f8df6a91881706f59f9a1b23655f5d1fbb683
fauzi fadillah <fauzifadilllah224@gmail.com>
2018-10-04 15:52:54 +0700

    changes

--- app/router.js
+++ app/router.js
@@ -19,13 +19,10 @@ Router.map(function() {
     this.route('edit',{path: '/edit/:karyawan_id'});
   });
 
-<<<<<<< HEAD
   this.route('kategori', function() {
     this.route('index');
   });
   this.route('departemen', function() {});
-=======
->>>>>>> 0b9a6f0a107c60ccae1db04faf79ddb09787dc17
 });
 
 export default Router;
